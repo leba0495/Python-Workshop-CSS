b8206d8d303151ac795e188e74d5600d8badc064
Luis B <61469904+leba0495@users.noreply.github.com>
2021-09-22 19:53:36 -0400

    Update carManager.py

--- carManager.py
+++ carManager.py
@@ -1,21 +1,20 @@
-"""Car manager class pupose"""
-
 # import turtle from Turtle
 
 # import random
 
 """Global Variables"""
 
-# List of colors
+# List of colors (to pick at random)
 
-# Moving distance of the cars
+# Starting Moving distance of the cars on each refresh
 
-# Moving increment of the cars
+# Moving increment of the cars 
 
 """Class declaration"""
 
 # class CarManager 
 
+"""Car manager class pupose is to generate all the random cars and move them across the screen """
     # Defining the class
 
         # List of all cars
